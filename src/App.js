74501d3ab0a9002e4afac65d40fd91f2e722dfcd
princetojo <trevon@andrews.edu>
2021-01-19 10:42:30 -0500

    Removed Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Home from './components/Home';
-import Schedule from './components/Schedule';
 
 function App() {
   return (
-    <Router basename={"aureccenter.github.io"}>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/employee-schedule" component={Schedule}/>
-      </Switch>
-    </Router>
+    <>
+      <Home />
+    </>
   );
 }
 
