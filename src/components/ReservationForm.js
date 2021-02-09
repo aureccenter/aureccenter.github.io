3ca0343b7e62b6e6fda70da3372ef8c19929c817
princetojo <trevon@andrews.edu>
2021-02-09 07:03:00 -0500

    Completed form

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -2,7 +2,8 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import TimePicker from 'react-bootstrap-time-picker';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const ReservationForm = () => {
   const [formResponses, setFormResponses] = useState({
@@ -15,23 +16,101 @@ const ReservationForm = () => {
     reservationDate: '',
     reservationDay: '',
     numberOfAttendees: '',
-    startTime: 28800,
-    endTime: 30600,
+    startTime: 39600,
+    endTime: 41400,
+    start: '11:00 AM',
+    end: '11:30 AM',
     organization: '',
+    isRecurring: false,
+    recurringFrequency: '',
   });
 
   const [isRecurring, setIsRecurring] = useState(false);
-  const [dayOfWeek, setDayOfWeek] = useState(0);
+  const [validated, setValidated] = useState(false);
+  const [timeIsBad, setTimeIsBad] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const inputRef = useRef();
+  const inputRef2 = useRef();
 
-  return (
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+    setFormSubmitted(true);
+
+    emailjs
+      .send(
+        'service_1vw76um',
+        'template_j575ef8',
+        formResponses,
+        'user_wJ5IIawclAEmWJ073b9vj'
+      )
+      .then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        }
+      );
+  };
+
+  const handleOnChange = (email, event) => {
+    // don't remember from where i copied this code, but this works.
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(email)) {
+      event.target.setCustomValidity('');
+    } else {
+      event.target.setCustomValidity('Email is invalid');
+    }
+  };
+
+  const convertTime = (number) => {
+    let newNumber = number / 3600;
+    let decimal = newNumber - Math.floor(newNumber);
+    newNumber = Math.floor(newNumber);
+    decimal *= 60;
+    let timeOfDay;
+    if (newNumber >= 12) timeOfDay = 'PM';
+    else timeOfDay = 'AM';
+    return [
+      `${newNumber == 0 || newNumber == 12 ? 12 : newNumber % 12}:${
+        decimal == 0 ? '00' : decimal
+      } ${timeOfDay}`,
+      `${newNumber}:${decimal == 0 ? '00' : decimal}`,
+    ];
+  };
+
+  return formSubmitted ? (
+    <div className='container'>
+      <div className='row'>
+        <h4 style={{ textAlign: 'center' }}>
+          {' '}
+          Thank you for your submission. You will be emailed at
+          <span style={{ color: 'cornflowerblue' }}>
+            {' '}
+            {formResponses.email}
+          </span>{' '}
+          shortly{' '}
+        </h4>
+        <div style={{ height: '400px' }} />
+      </div>
+    </div>
+  ) : (
     <div className='container'>
       <div className='row'>
         <div className='col'></div>
         <div className='col-9'>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group controlId='formBasicEmail'>
               <Form.Label className='mt-1'>First Name</Form.Label>
               <Form.Control
+                required
                 type='text'
                 placeholder='Enter First Name'
                 onChange={(e) => {
@@ -44,6 +123,7 @@ const ReservationForm = () => {
 
               <Form.Label className='mt-1'>Last Name</Form.Label>
               <Form.Control
+                required
                 type='text'
                 placeholder='Enter Last Name'
                 onChange={(e) => {
@@ -53,21 +133,29 @@ const ReservationForm = () => {
                   });
                 }}
               />
-
+            </Form.Group>
+            <Form.Group>
               <Form.Label className='mt-1'>Email</Form.Label>
               <Form.Control
+                required
                 type='text'
                 placeholder='Enter Email Address'
                 onChange={(e) => {
+                  handleOnChange(e.target.value, e);
                   setFormResponses({
                     ...formResponses,
                     email: e.target.value,
                   });
                 }}
               />
-
+              <Form.Control.Feedback type='invalid'>
+                Invalid Email
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group>
               <Form.Label className='mt-1'>ID Number</Form.Label>
               <Form.Control
+                required
                 type='number'
                 placeholder='Enter ID Number'
                 min='0'
@@ -83,6 +171,7 @@ const ReservationForm = () => {
                 What Is the Reservation For?
               </Form.Label>
               <Form.Control
+                required
                 type='text'
                 placeholder='Ex. Birthday Party, Club Name Etc.'
                 onChange={(e) => {
@@ -98,6 +187,7 @@ const ReservationForm = () => {
               <Form.Label>Which Area Would You Like to Reserve?</Form.Label>
               <hr />
               <Form.Check
+                required={formResponses.rooms.length === 0}
                 type='checkbox'
                 label='Main Rec Center'
                 onClick={(e) => {
@@ -118,6 +208,7 @@ const ReservationForm = () => {
               />
               <Form.Text className='text-muted mb-2'>Capacity 10</Form.Text>
               <Form.Check
+                required={formResponses.rooms.length === 0}
                 type='checkbox'
                 label='Amphitheater'
                 onClick={(e) => {
@@ -140,6 +231,7 @@ const ReservationForm = () => {
                 Capacity 15 (If socially distanced chairs are used)
               </Form.Text>
               <Form.Check
+                required={formResponses.rooms.length === 0}
                 type='checkbox'
                 label='Study/Conference Room'
                 onClick={(e) => {
@@ -169,6 +261,7 @@ const ReservationForm = () => {
                 What Date would you Like to Reserve?
               </Form.Label>
               <Form.Control
+                required
                 type='date'
                 onChange={(e) => {
                   if (e.target.valueAsDate === null) {
@@ -179,17 +272,19 @@ const ReservationForm = () => {
                     });
                   } else {
                     let dayOfWeek = new Date(
-                      e.target.value + '00:00:00'
-                    ).getDay();
+                      e.target.valueAsDate.getTime() +
+                        e.target.valueAsDate.getTimezoneOffset() * 60000
+                    )
+                      .toString()
+                      .substring(0, 3);
                     setFormResponses({
                       ...formResponses,
                       reservationDate: e.target.value,
-                      reservationDay: new Date(
-                        e.target.valueAsDate.getTime() +
-                          e.target.valueAsDate.getTimezoneOffset() * 60000
-                      )
-                        .toString()
-                        .substring(0, 3),
+                      recurringFrequency:
+                        dayOfWeek == 'Sat'
+                          ? 'Once a month'
+                          : formResponses.recurringFrequency,
+                      reservationDay: dayOfWeek,
                     });
                   }
                 }}
@@ -199,12 +294,17 @@ const ReservationForm = () => {
                 Is This Reservation Recurring?
               </Form.Label>
               <Form.Check
+                required
                 type='radio'
                 label='Yes'
                 name='formQuestionRadios'
                 id='formQuestionRadios1'
-                onChange={(e) => {
+                onChange={() => {
                   setIsRecurring(true);
+                  setFormResponses({
+                    ...formResponses,
+                    isRecurring: true,
+                  });
                 }}
               />
               <Form.Check
@@ -212,8 +312,12 @@ const ReservationForm = () => {
                 label='No'
                 name='formQuestionRadios'
                 id='formQuestionRadios2'
-                onChange={(e) => {
+                onChange={() => {
                   setIsRecurring(false);
+                  setFormResponses({
+                    ...formResponses,
+                    isRecurring: false,
+                  });
                 }}
               />
 
@@ -222,7 +326,21 @@ const ReservationForm = () => {
                   <Form.Label className='mt-1'>
                     How Often Does it Occur?
                   </Form.Label>
-                  <Form.Control as='select'>
+
+                  <Form.Control
+                    required
+                    as='select'
+                    onChange={(e) => {
+                      setFormResponses({
+                        ...formResponses,
+                        recurringFrequency: e.target.value,
+                      });
+                    }}
+                  >
+                    <option disabled selected value>
+                      {' '}
+                      -- select an option --{' '}
+                    </option>
                     {formResponses.reservationDay !== 'Sat' ? (
                       <>
                         <option>Every week</option>
@@ -242,6 +360,7 @@ const ReservationForm = () => {
                 How Many People would be Attending the Reservation?
               </Form.Label>
               <Form.Control
+                required
                 type='number'
                 placeholder='Enter a number'
                 min='0'
@@ -253,48 +372,101 @@ const ReservationForm = () => {
                 }}
               />
             </Form.Group>
-
-            <Form.Label className='mt-1'>
-              What Time Would you Like to Reserve??
-            </Form.Label>
-            <Form.Row>
-              <Col>
-                <Form.Label column='sm'>Start Time</Form.Label>
-                <TimePicker
-                  start='8:00'
-                  end='23:30'
-                  value={formResponses.startTime}
-                  onChange={(time) => {
-                    setFormResponses({
-                      ...formResponses,
-                      startTime: time,
-                    });
-                  }}
-                  step={30}
-                />
-              </Col>
-              <Col>
-                <Form.Label column='sm'>End Time</Form.Label>
-                <TimePicker
-                  start='8:00'
-                  end='23:30'
-                  value={formResponses.endTime}
-                  onChange={(time) => {
-                    setFormResponses({
-                      ...formResponses,
-                      endTime: time,
-                    });
-                  }}
-                  step={30}
-                />
-              </Col>
-            </Form.Row>
+            <Form.Group>
+              <Form.Label className='mt-1'>
+                What Time Would you Like to Reserve??
+              </Form.Label>
+              <Form.Control
+                required={timeIsBad}
+                type='text'
+                style={{ display: 'none' }}
+              />
+              <Form.Control.Feedback type='invalid'>
+                {errorMessage}
+              </Form.Control.Feedback>
+              <Form.Row>
+                <Col>
+                  <Form.Label column='sm'>Start Time</Form.Label>
+                  <div ref={inputRef}>
+                    <TimePicker
+                      start='11:00'
+                      value={formResponses.startTime}
+                      onChange={(time) => {
+                        setFormResponses({
+                          ...formResponses,
+                          startTime: time,
+                          endTime: time == 84600 ? 0 : time + 1800,
+                          start: convertTime(time)[0],
+                          end: convertTime(time == 84600 ? 0 : time + 1800)[0],
+                        });
+                        if (time >= formResponses.endTime) {
+                          inputRef.current.firstChild.setCustomValidity(
+                            'Time is invalid'
+                          );
+                          inputRef2.current.firstChild.setCustomValidity(
+                            'Time is invalid'
+                          );
+                          setTimeIsBad(true);
+                          setErrorMessage(
+                            'The end time must be after the start time'
+                          );
+                        } else {
+                          setTimeIsBad(false);
+                          inputRef.current.firstChild.setCustomValidity('');
+                          inputRef2.current.firstChild.setCustomValidity('');
+                        }
+                      }}
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <Form.Label column='sm'>End Time</Form.Label>
+                  <div ref={inputRef2}>
+                    <TimePicker
+                      start={
+                        formResponses.startTime !== 84600
+                          ? convertTime(formResponses.startTime + 1800)[1]
+                          : '00:00'
+                      }
+                      end={
+                        formResponses.startTime !== 84600 ? '23:30' : '00:00'
+                      }
+                      value={formResponses.endTime}
+                      onChange={(time) => {
+                        setFormResponses({
+                          ...formResponses,
+                          endTime: time,
+                          end: convertTime(time)[0],
+                        });
+                        if (time <= formResponses.startTime) {
+                          inputRef2.current.firstChild.setCustomValidity(
+                            'Time is invalid'
+                          );
+                          inputRef.current.firstChild.setCustomValidity(
+                            'Time is invalid'
+                          );
+                          setTimeIsBad(true);
+                          setErrorMessage(
+                            'The end time must be after the start time'
+                          );
+                        } else {
+                          setTimeIsBad(false);
+                          inputRef2.current.firstChild.setCustomValidity('');
+                          inputRef.current.firstChild.setCustomValidity('');
+                        }
+                      }}
+                    />
+                  </div>
+                </Col>
+              </Form.Row>
+            </Form.Group>
 
             <Form.Group controlId='formOrganization'>
               <Form.Label className='mt-2'>
                 Is There An Organization Associated with Your Reservation?
               </Form.Label>
               <Form.Control
+                required
                 type='text'
                 placeholder='Ex. BSCF, AUSA'
                 onChange={(e) => {
